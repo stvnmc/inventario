bed74e156e683694df2c4d81329e233f3c4ee778
stvnmc <stvnmc123@gmail.com>
2025-08-10 11:24:46 -0600

    creando el sistema de reservaciones

--- src/components/admin/componentsReservationAdmin/Reservation.jsx
+++ src/components/admin/componentsReservationAdmin/Reservation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiUserMinus } from "react-icons/bi";
 import { BiUserPlus } from "react-icons/bi";
 import { BiUserCheck } from "react-icons/bi";
@@ -11,6 +11,14 @@ const Reservation = ({
 }) => {
   const editReservation = (index, item) => {
     console.log(index, item);
+  };
+
+  const [showEditform, setShowEditform] = useState(true);
+  const [indexShow, setindexShow] = useState(null);
+
+  const openEditReservation = (index) => {
+    setindexShow(index);
+    setShowEditform(!showEditform);
   };
 
   return (
@@ -26,9 +34,7 @@ const Reservation = ({
               <button>
                 <BiUserCheck />
               </button>
-              <button
-                onClick={() => editReservation(index, item.placeAndPeople.time)}
-              >
+              <button onClick={() => openEditReservation(index)}>
                 <BiUserPlus />
               </button>
 
@@ -40,39 +46,45 @@ const Reservation = ({
                 <BiUserMinus />
               </button>
             </div>
-
-            {/* <h2>{item.personalInformation.nameClient}</h2>
-            <div>
-              <h3>{item.personalInformation.phoneClient}</h3>
-            </div>
-            <div>
-              <div>{item.placeAndPeople.time}</div>
-              <div>{item.placeAndPeople.people}</div>
-              <div>{item.placeAndPeople.place}</div>
-            </div> */}
-            <div>{item.personalInformation.mailClient}</div>
-            <form>
-              <h2>name</h2>
-              <input value={item.personalInformation.nameClient}></input>
-              <div>
-                <h3>phone</h3>
-                <input value={item.personalInformation.phoneClient}></input>
-              </div>
-              <div>
-                <div>time</div>
-                <input value={item.placeAndPeople.time}></input>
-                <div>people</div>
-                <input value={item.placeAndPeople.people}></input>
-                <div>place</div>
-                <input value={item.placeAndPeople.place}></input>
-              </div>
-              {item.personalInformation.mailClient ? (
-                <>
-                  <div>gmail</div>
-                  <input value={item.personalInformation.mailClient}></input>
-                </>
-              ) : null}
-            </form>
+            {showEditform && index === indexShow ? (
+              <form>
+                <h2>name</h2>
+                <input value={item.personalInformation.nameClient}></input>
+                <div>
+                  <h3>phone</h3>
+                  <input value={item.personalInformation.phoneClient}></input>
+                </div>
+                <div>
+                  <div>time</div>
+                  <input value={item.placeAndPeople.time}></input>
+                  <div>people</div>
+                  <input value={item.placeAndPeople.people}></input>
+                  <div>place</div>
+                  <input value={item.placeAndPeople.place}></input>
+                  {item.personalInformation.mailClient ? (
+                    <>
+                      <div>gmail</div>
+                      <input
+                        value={item.personalInformation.mailClient}
+                      ></input>
+                    </>
+                  ) : null}
+                </div>
+              </form>
+            ) : (
+              <>
+                <h2>{item.personalInformation.nameClient}</h2>
+                <div>
+                  <h3>{item.personalInformation.phoneClient}</h3>
+                </div>
+                <div>
+                  <div>{item.placeAndPeople.time}</div>
+                  <div>{item.placeAndPeople.people}</div>
+                  <div>{item.placeAndPeople.place}</div>
+                  <div>{item.personalInformation.mailClient}</div>
+                </div>
+              </>
+            )}
           </div>
         ))}
     </div>
